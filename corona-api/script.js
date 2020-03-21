d686bc3e5dbc32ec8d9ff26ff6ac2195a98d1fe7
Douglas Cananéa <cananeadouglas@gmail.com>
2020-03-21 00:54:41 -0300

    coronaVirus API 2

--- corona-api/script.js
+++ corona-api/script.js
@@ -43,20 +43,20 @@ function manda(){
     let casos = window.document.getElementById('casos')
     let mortes = window.document.getElementById('mortes')
     let recupera = window.document.getElementById('recupera')
+    document.body.style.background = '#e7ca0f'
+    let pais = "Brazil"
 
     axios.get('https://corona.lmao.ninja/all')
         .then(function(response){
             let chega = console.log(response.data);
             casos.innerHTML = `Casos confirmados: ${response.data.cases}`
             mortes.innerHTML = `Números de mortos: ${response.data.deaths}`
-            recupera.innerHTML = `Pessoas recuperadas: ${response.data.recovered}`
-            
+            recupera.innerHTML = `Pessoas recuperadas: ${response.data.recovered}`  
         })
         .catch(function(error) {
             console.warn(error);
         })
-
-
+        
         let titulo = window.document.getElementById('titulo')
         let casos1 = window.document.getElementById('casos1')
         let mortes1 = window.document.getElementById('mortes1')
@@ -64,15 +64,25 @@ function manda(){
         let ativos = window.document.getElementById('ativos')
         let criticos = window.document.getElementById('criticos')
 
-    axios.get('https://corona.lmao.ninja/countries')
-        .then(function(response){
-            let chega = console.log(response.data[24]);
-            titulo.innerHTML = `Números do Coronavirus no ${response.data[24].country}`
-            casos1.innerHTML = `Casos confirmados: ${response.data[24].cases}`
-            mortes1.innerHTML = `Número de mortos: ${response.data[24].deaths}`
-            recupera1.innerHTML = `Pessoas recuperadas: ${response.data[24].recovered}`
-            ativos.innerHTML = `Pessoas ativas: ${response.data[24].active}`
-            criticos.innerHTML = `Casos criticos: ${response.data[24].critical}`
+        axios.get('https://corona.lmao.ninja/countries')
+            .then(function(response){
+            console.log(response);
+           
+                for(i = 0; i <= 99; i++){
+                    if (pais == response.data[i].country){
+                        console.log(pais)
+                        titulo.innerHTML = `Números do Coronavirus ${response.data[i].country}`
+                        casos1.innerHTML = `Casos confirmados: ${response.data[i].cases}`
+                        mortes1.innerHTML = `Número de mortos: ${response.data[i].deaths}`
+                        recupera1.innerHTML = `Pessoas recuperadas: ${response.data[i].recovered}`
+                        ativos.innerHTML = `Pessoas ativas: ${response.data[i].active}`
+                        criticos.innerHTML = `Casos criticos: ${response.data[i].critical}`
+                    }
+                    else{
+                        //titulo.innerHTML = 'error'
+                    }
+                }
+            
             
         })
         .catch(function(error) {
