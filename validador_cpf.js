3a3834878360ba4bd69b46ef8ace8fa6c5bbb549
Douglas Cananéa <cananeadouglas@gmail.com>
2020-05-14 22:24:24 -0300

    detector de navegadores, java_enable

--- validador_cpf.js
+++ validador_cpf.js
@@ -1,16 +1,27 @@
-/*function validar(){
-    
-    var str = "lkjlk";
-    var strReverse = str.split('').join('')
-    console.log(strReverse[2])
 
+var stringExemplo = "12345678900";
+var resultado = stringExemplo.substring(0, 10);
 
-}*/
+var a = stringExemplo.substring(0,1)
+var b = stringExemplo.substring(1,2)
+var c = stringExemplo.substring(2,3)
+var d = stringExemplo.substring(3,4)
+var e = stringExemplo.substring(4,5)
+var f = stringExemplo.substring(5,6)
+var g = stringExemplo.substring(6,7)
+var h = stringExemplo.substring(7,8)
+var i = stringExemplo.substring(8,9)
+var j = stringExemplo.substring(9,10)
+var l = stringExemplo.substring(10,11)
 
-var numer = "f21323d"
-//console.log(numer.substring(2,3))
-
-n1 = numer.substr(0)
-n2 = numer.substr(1)
-
-console.log(n1 + n2)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+console.log(l)
