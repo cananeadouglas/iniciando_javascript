5ae7eeff08e232104d994009e1f0bc29836757aa
Douglas Cananéa <cananeadouglas@gmail.com>
2023-03-11 01:06:29 -0300

    Atualizado em 11.03.2023. cotacao_crypto

---
+++ consulta_cep/script.js
@@ -0,0 +1,56 @@
+function buscarCEP() {
+    const input = document.getElementById('cep').value;
+    const apiurl = 'https://cep.awesomeapi.com.br/json/';
+    const endpoint = apiurl + input;
+
+    
+
+    axios.get(endpoint).then(response => {
+        
+        const cepResultado = document.getElementById('cepp');
+        cepResultado.textContent = `CEP digitado: ${response.data.cep}`; // atribui o valor do CEP ao conteúdo do elemento
+        
+        const tipo = document.getElementById('tipo');
+        tipo.textContent = `Tipo: ${response.data.address_type}`;
+
+        const rua = document.getElementById('rua');
+        rua.textContent = `Endereco: ${response.data.address}`;
+
+        const bairro = document.getElementById('bairro');
+        bairro.textContent = `Bairro: ${response.data.district}`;
+        
+        const city = document.getElementById('city');
+        city.textContent = `Cidade: ${response.data.city}`;
+
+        const city_ibge = document.getElementById('city_ibge');
+        city_ibge.textContent = `Habitantes: ${response.data.city_ibge}`;
+
+        const ddd = document.getElementById('ddd');
+        ddd.textContent = `ddd: ${response.data.ddd}`;
+
+        const lat = document.getElementById('lat');
+        lat.textContent = `Latitude: ${response.data.lat}`;
+
+        const lng = document.getElementById('lng');
+        lng.textContent = `Longitude: ${response.data.lng}`;
+        
+        /*
+        const googl = 'https://www.google.com.br/maps/@'+`${response.data.lat}`+`,`+`${response.data.lng}`;
+        
+        const btn = document.querySelector('#btn')
+
+        function openInNewTab(googl){
+            const win = window.open(googl, '_blank')
+            win.focus()
+        }
+
+        btn.addEventListener('click', () => {
+            openInNewTab(googl)
+        })
+        */
+
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
